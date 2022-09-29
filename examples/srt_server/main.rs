ed960b537139efce316319b89445de71c42211f5
aegroto <aegroto@protonmail.com>
2022-09-29 10:09:53 +0200

    complete encoding-decoding flow, although frame delay seems too high

--- examples/srt_server/main.rs
+++ examples/srt_server/main.rs
@@ -20,6 +20,8 @@ use remotia::{
     processors::{containers::sequential::Sequential, ticker::Ticker},
     scrap::ScrapFrameCapturer,
 };
+use remotia_ffmpeg_codecs::decoders::h264::H264Decoder;
+use remotia_ffmpeg_codecs::encoders::x264::X264Encoder;
 use scrap::{Capturer, Display};
 
 #[tokio::main]
@@ -37,6 +39,7 @@ async fn main() -> std::io::Result<()> {
     pools.register("y_channel_buffer", 8, width * height);
     pools.register("cr_channel_buffer", 8, (width * height) / 4);
     pools.register("cb_channel_buffer", 8, (width * height) / 4);
+    pools.register("encoded_frame_buffer", 24, width * height * 4);
 
     let mut pipelines = PipelineRegistry::new();
 
@@ -87,7 +90,7 @@ fn capturer(pools: &mut PoolRegistry, display_id: usize) -> impl FrameProcessor 
     let capturer = ScrapFrameCapturer::new(Capturer::new(display).unwrap());
 
     Sequential::new()
-        .append(Ticker::new(30))
+        .append(Ticker::new(1000))
         .append(time_start!("capture_idle"))
         .append(pools.get("raw_frame_buffer").borrower())
         .append(time_diff!("capture_idle"))
@@ -98,7 +101,15 @@ fn capturer(pools: &mut PoolRegistry, display_id: usize) -> impl FrameProcessor 
 }
 
 fn encoder(pools: &mut PoolRegistry, pipelines: &mut PipelineRegistry) -> impl FrameProcessor {
+    // TODO: Make these configurable
+    let width = 1280;
+    let height = 720;
+
     Sequential::new()
+        .append(TimestampDiffCalculator::new(
+            "capture_timestamp",
+            "encode_delay",
+        ))
         .append(time_start!("encode_idle"))
         .append(pools.get("y_channel_buffer").borrower())
         .append(pools.get("cb_channel_buffer").borrower())
@@ -109,17 +120,37 @@ fn encoder(pools: &mut PoolRegistry, pipelines: &mut PipelineRegistry) -> impl F
         .append(time_start!("encode_processing"))
         .append(RGBAToYUV420PConverter::new())
         .append(pools.get("raw_frame_buffer").redeemer())
+        .append(pools.get("encoded_frame_buffer").borrower())
+        .append(X264Encoder::new(
+            width * height * 4,
+            width as i32,
+            height as i32,
+            "keyint=16",
+        ))
+        .append(pools.get("y_channel_buffer").redeemer())
+        .append(pools.get("cb_channel_buffer").redeemer())
+        .append(pools.get("cr_channel_buffer").redeemer())
         .append(time_diff!("encode_processing"))
 }
 
 fn decoder(pools: &mut PoolRegistry, pipelines: &mut PipelineRegistry) -> impl FrameProcessor {
     Sequential::new()
+        .append(TimestampDiffCalculator::new(
+            "capture_timestamp",
+            "decode_delay",
+        ))
+
         .append(time_start!("decode_idle"))
         .append(pools.get("raw_frame_buffer").borrower())
         .append(OnErrorSwitch::new(pipelines.get_mut("error")))
         .append(time_diff!("decode_idle"))
 
         .append(time_start!("decode_processing"))
+        .append(pools.get("y_channel_buffer").borrower())
+        .append(pools.get("cb_channel_buffer").borrower())
+        .append(pools.get("cr_channel_buffer").borrower())
+        .append(H264Decoder::new())
+        .append(pools.get("encoded_frame_buffer").redeemer())
         .append(YUV420PToRGBAConverter::new())
         .append(pools.get("y_channel_buffer").redeemer())
         .append(pools.get("cb_channel_buffer").redeemer())
@@ -133,7 +164,7 @@ fn renderer(pools: &mut PoolRegistry, pipelines: &mut PipelineRegistry) -> impl 
             "capture_timestamp",
             "frame_delay",
         ))
-        .append(ThresholdBasedFrameDropper::new("frame_delay", 40))
+        .append(ThresholdBasedFrameDropper::new("frame_delay", 2000))
         .append(OnErrorSwitch::new(pipelines.get_mut("error")))
         .append(time_start!("render_processing"))
         .append(BerylliumRenderer::new(1280, 720))
