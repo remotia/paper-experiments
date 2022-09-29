b71aca55d4ac26853827c2e76aab0a320e14c832
aegroto <aegroto@protonmail.com>
2022-09-29 16:41:16 +0200

    moved components initialization functions to a common module

--- examples/auto_encoding/main.rs
+++ examples/auto_encoding/main.rs
@@ -1,35 +1,18 @@
-use std::path::PathBuf;
-
-use log::debug;
-use paper_experiments::dumper;
+use paper_experiments::common::{decoder, renderer, capturer, encoder};
 use paper_experiments::panicker::Panicker;
 use paper_experiments::pipeline_registry::PipelineRegistry;
 use paper_experiments::register;
-use paper_experiments::time_diff;
-use paper_experiments::time_start;
 use paper_experiments::utils::printer;
 
-use paper_experiments::yuv_to_rgba::YUV420PToRGBAConverter;
 use remotia::csv::serializer::CSVFrameDataSerializer;
-use remotia::frame_dump::RawFrameDumper;
-use remotia::processors::error_switch::OnErrorSwitch;
-use remotia::processors::frame_drop::threshold::ThresholdBasedFrameDropper;
-use remotia::processors::functional::Function;
 use remotia::processors::switch::Switch;
-use remotia::time::add::TimestampAdder;
-use remotia::time::diff::TimestampDiffCalculator;
+use remotia::processors::ticker::Ticker;
 use remotia::traits::FrameProcessor;
-use remotia::yuv420p::encoder::RGBAToYUV420PConverter;
 use remotia::{
-    beryllium::BerylliumRenderer,
     pipeline::ascode::{component::Component, AscodePipeline},
     pool_registry::PoolRegistry,
-    processors::{containers::sequential::Sequential, ticker::Ticker},
-    scrap::ScrapFrameCapturer,
+    processors::{containers::sequential::Sequential},
 };
-use remotia_ffmpeg_codecs::decoders::h264::H264Decoder;
-use remotia_ffmpeg_codecs::encoders::x264::X264Encoder;
-use scrap::{Capturer, Display};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -77,6 +60,7 @@ async fn main() -> std::io::Result<()> {
         "encoding",
         AscodePipeline::new().link(
             Component::new()
+                .append(Ticker::new(30))
                 .append(capturer(&mut pools, display_id))
                 .append(encoder(&mut pools, &mut pipelines))
                 .append(Switch::new(pipelines.get_mut("decoding")))
@@ -113,91 +97,4 @@ fn logger() -> impl FrameProcessor {
                 .log("decode_delay")
                 .log("frame_delay"),
         )
-}
-
-fn capturer(pools: &mut PoolRegistry, display_id: usize) -> impl FrameProcessor {
-    let mut displays = Display::all().unwrap();
-    debug!("Displays: {:?}", displays.len());
-    let display = displays.remove(display_id);
-
-    Sequential::new()
-        .append(Panicker::new(5))
-        .append(Ticker::new(500))
-        .append(time_start!("capture_idle"))
-        .append(pools.get("raw_frame_buffer").borrower())
-        .append(time_diff!("capture_idle"))
-        .append(time_start!("capture_processing"))
-        .append(TimestampAdder::new("capture_timestamp"))
-        .append(ScrapFrameCapturer::new(Capturer::new(display).unwrap()))
-        .append(time_diff!("capture_processing"))
-        .append(dumper!("raw_frame_buffer", "dump/input_frames"))
-}
-
-fn encoder(pools: &mut PoolRegistry, pipelines: &mut PipelineRegistry) -> impl FrameProcessor {
-    // TODO: Make these configurable
-    let width = 1280;
-    let height = 720;
-
-    let encoder = X264Encoder::new(width as i32, height as i32, "keyint=16");
-
-    Sequential::new()
-        .append(TimestampDiffCalculator::new(
-            "capture_timestamp",
-            "encode_delay",
-        ))
-        .append(time_start!("encode_idle"))
-        .append(pools.get("y_channel_buffer").borrower())
-        .append(pools.get("cb_channel_buffer").borrower())
-        .append(pools.get("cr_channel_buffer").borrower())
-        .append(OnErrorSwitch::new(pipelines.get_mut("error")))
-        .append(time_diff!("encode_idle"))
-        .append(time_start!("encode_processing"))
-        .append(RGBAToYUV420PConverter::new())
-        .append(pools.get("raw_frame_buffer").redeemer())
-        .append(pools.get("encoded_frame_buffer").borrower())
-        .append(encoder.pusher())
-        .append(pools.get("y_channel_buffer").redeemer())
-        .append(pools.get("cb_channel_buffer").redeemer())
-        .append(pools.get("cr_channel_buffer").redeemer())
-        .append(encoder.puller())
-        .append(time_diff!("encode_processing"))
-}
-
-fn decoder(pools: &mut PoolRegistry, pipelines: &mut PipelineRegistry) -> impl FrameProcessor {
-    Sequential::new()
-        .append(TimestampDiffCalculator::new(
-            "capture_timestamp",
-            "decode_delay",
-        ))
-        .append(time_start!("decode_idle"))
-        .append(pools.get("raw_frame_buffer").borrower())
-        .append(OnErrorSwitch::new(pipelines.get_mut("error")))
-        .append(time_diff!("decode_idle"))
-        .append(time_start!("decode_processing"))
-        .append(pools.get("y_channel_buffer").borrower())
-        .append(pools.get("cb_channel_buffer").borrower())
-        .append(pools.get("cr_channel_buffer").borrower())
-        .append(H264Decoder::new())
-        .append(OnErrorSwitch::new(pipelines.get_mut("error")))
-        .append(pools.get("encoded_frame_buffer").redeemer())
-        .append(YUV420PToRGBAConverter::new())
-        .append(pools.get("y_channel_buffer").redeemer())
-        .append(pools.get("cb_channel_buffer").redeemer())
-        .append(pools.get("cr_channel_buffer").redeemer())
-        .append(time_diff!("decode_processing"))
-        .append(dumper!("raw_frame_buffer", "dump/decoded_frames"))
-}
-
-fn renderer(pools: &mut PoolRegistry, pipelines: &mut PipelineRegistry) -> impl FrameProcessor {
-    Sequential::new()
-        .append(TimestampDiffCalculator::new(
-            "capture_timestamp",
-            "frame_delay",
-        ))
-        .append(ThresholdBasedFrameDropper::new("frame_delay", 20000))
-        .append(OnErrorSwitch::new(pipelines.get_mut("error")))
-        .append(time_start!("render_processing"))
-        .append(BerylliumRenderer::new(1280, 720))
-        .append(pools.get("raw_frame_buffer").redeemer())
-        .append(time_diff!("render_processing"))
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod common;
 pub mod utils;
 
 // TODO: To be moved
