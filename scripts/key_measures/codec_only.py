5553a5eb265d6b1b9ed6200348886f3fa02bcf96
aegroto <aegroto@protonmail.com>
2022-11-10 11:38:17 +0100

    improved key measures script

--- scripts/key_measures/codec_only.py
+++ scripts/key_measures/codec_only.py
@@ -8,31 +8,34 @@ root_folder = sys.argv[1]
 summary = list()
 
 for folder_path in os.listdir(root_folder):
-    row = list()
+    try:
+        row = list()
 
-    config = toml.load(open(f"{root_folder}/{folder_path}/{folder_path}"))
-    row.append(config['video_file_path'])
-    row.append(config['encoder_options']['crf'])
+        config = toml.load(open(f"{root_folder}/{folder_path}/{folder_path}"))
+        row.append(config['video_file_path'])
+        row.append(config['encoder_options']['crf'])
 
-    codec_stats = pandas.read_csv(f"{root_folder}/{folder_path}/results/stats/codec.csv")
-    row.append(codec_stats['encoded_size'].mean())
+        codec_stats = pandas.read_csv(f"{root_folder}/{folder_path}/results/stats/codec.csv")
+        row.append(codec_stats['encoded_size'].mean())
 
-    delay_stats = pandas.read_csv(f"{root_folder}/{folder_path}/results/stats/delay.csv")
-    row.append(delay_stats['frame_delay'].mean())
+        delay_stats = pandas.read_csv(f"{root_folder}/{folder_path}/results/stats/delay.csv")
+        row.append(delay_stats['frame_delay'].mean())
 
-    # processing_stats = pandas.read_csv(f"{root_folder}/{folder_path}/results/stats/processing.csv")
-    # row.append(processing_stats['capture_processing_time'].mean())
-    # row.append(processing_stats['yuv420p_conversion_processing_time'].mean())
-    # row.append(processing_stats['encode_processing_time'].mean())
-    # row.append(processing_stats['decode_processing_time'].mean())
-    # row.append(processing_stats['rgba_conversion_processing_time'].mean())
+        # processing_stats = pandas.read_csv(f"{root_folder}/{folder_path}/results/stats/processing.csv")
+        # row.append(processing_stats['capture_processing_time'].mean())
+        # row.append(processing_stats['yuv420p_conversion_processing_time'].mean())
+        # row.append(processing_stats['encode_processing_time'].mean())
+        # row.append(processing_stats['decode_processing_time'].mean())
+        # row.append(processing_stats['rgba_conversion_processing_time'].mean())
 
-    vmaf_stats = pandas.read_csv(f"{root_folder}/{folder_path}/results/vmaf.csv")
-    row.append(vmaf_stats['psnr_hvs'].mean())
-    row.append(vmaf_stats['float_ssim'].mean())
-    row.append(vmaf_stats['vmaf'].mean())
+        vmaf_stats = pandas.read_csv(f"{root_folder}/{folder_path}/results/vmaf.csv")
+        row.append(vmaf_stats['psnr_hvs'].mean())
+        row.append(vmaf_stats['float_ssim'].mean())
+        row.append(vmaf_stats['vmaf'].mean())
 
-    summary.append(row)
+        summary.append(row)
+    except Exception as e:
+        print(f"Unable to read {folder_path}: {e}")
 
 df_summary = pandas.DataFrame(summary, columns = [
     "video_path", 
@@ -48,6 +51,7 @@ df_summary = pandas.DataFrame(summary, columns = [
     "float_ssim",
     "vmaf"
 ])
+
 by_crf_summary = df_summary.groupby("crf").mean()
 by_crf_summary.to_csv(sys.argv[2])
 print(by_crf_summary)
